a01fdc2c1c85015bd027d76c38e39406d6eae782
teekay <olajideg2@yahoo.com>
2022-11-22 21:14:55 -0500

    my file.tf

--- file.tf
+++ file.tf
@@ -1,13 +1,32 @@
 provider "aws" {
   region = "us-east-1"
-access_key = "AKIAYKZF26WRLU7GJIF2"
-secret_key = "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"
+access_key = "AKIAYKZF26WRDULF37OL"
+secret_key = "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"
 }
-resource "aws_instance" "web" {
+
+resource "aws_instance" "BackendDev" {
   ami           = "ami-0b0dcb5067f052a63"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "terraworld"
+    Name = "ping"
+  }
+}
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
   }
 }
